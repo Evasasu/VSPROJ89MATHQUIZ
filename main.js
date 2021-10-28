dd63b883a4e53ec0c74e697aa1ca1732b4f7c3f2
Evasasu <80681272+Evasasu@users.noreply.github.com>
2021-10-28 15:11:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-	player1_name = localStorage.getItem("player1_name");
+player1_name = localStorage.getItem("player1_name");
 	player2_name = localStorage.getItem("player2_name");
 
 	player1_score = 0;
